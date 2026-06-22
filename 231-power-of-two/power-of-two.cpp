4cc82c21eb36e4ebc9afac63392850e49a9f28c7
zubairalmamoon2006-dev <zubairalmamoon2006@gmail.com>
2026-06-22 11:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.02%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n>1){
+            if(n%2!=0){
+                return false;
+            }
+        return isPowerOfTwo(n/2);
+        }
+        return true;
+
+        
+    }
+};
