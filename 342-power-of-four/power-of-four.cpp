bc851664ef512782d000362143a0b323ac729e86
zubairalmamoon2006-dev <zubairalmamoon2006@gmail.com>
2026-06-22 11:33:16 +0530

    Time: 3 ms (3.22%) | Memory: 7.7 MB (79.75%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else{
+            if(n%4==0){
+                return isPowerOfFour(n/4);
+            }
+            else{
+                return false;
+            }
+        } 
+        
+    }
+};
