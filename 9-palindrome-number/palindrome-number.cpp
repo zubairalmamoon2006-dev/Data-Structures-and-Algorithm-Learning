9640f38c312c7cb6722c14fcec0ee7a01738be5d
zubairalmamoon2006-dev <zubairalmamoon2006@gmail.com>
2026-06-30 10:32:46 +0530

    Time: 8 ms (10.14%) | Memory: 12.3 MB (5.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        vector<int> arr;
+        for(int i=0; x>0; i++){
+            arr.push_back(x%10);
+            x=x/10;
+        }
+        bool flag=true;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]!=arr[arr.size()-1-i]){
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+};
