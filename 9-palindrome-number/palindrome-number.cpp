3583069f6c11dbed970d5f0a391c538c79938a39
zubairalmamoon2006-dev <zubairalmamoon2006@gmail.com>
2026-06-30 10:41:17 +0530

    Time: 18 ms (6.04%) | Memory: 12.1 MB (5.00%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,18 +4,28 @@ public:
         if(x<0){
             return false;
         }
+        if(x==0){
+            return true;
+        }
         vector<int> arr;
         for(int i=0; x>0; i++){
             arr.push_back(x%10);
             x=x/10;
         }
-        bool flag=true;
+        // bool flag=true;
+        // for(int i=0; i<arr.size()/2; i++){
+        //     if(arr[i]!=arr[arr.size()-1-i]){
+        //         flag=false;
+        //         break;
+        //     }
+        // }
+        // return flag;
         for(int i=0; i<arr.size()/2; i++){
             if(arr[i]!=arr[arr.size()-1-i]){
-                flag=false;
-                break;
+                return false;
             }
         }
-        return flag;
+        return true;
+        
     }
 };
