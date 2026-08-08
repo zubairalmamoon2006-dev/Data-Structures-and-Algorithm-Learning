6ed5ee2d90e930ce5ec0374727f6be237ed4c76d
zubairalmamoon2006-dev <zubairalmamoon2006@gmail.com>
2026-08-08 23:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (36.22%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,25 +5,67 @@ public:
         if(n==1){
             return nums[0];
         }
-        else{
-            for(int i=0; i<n; i++){
-                if(i==0){
-                    if(nums[i]!=nums[i+1]){
-                        return nums[i];
-                    }
-                }
-                else if(i==n-1){
-                    if(nums[i]!=nums[i-1]){
-                        return nums[i];
-                    }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            else if(nums[mid]!=nums[mid-1] && nums[mid]==nums[mid+1]){
+                if(mid%2==0){
+                    low=mid+1;
                 }
                 else{
-                    if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                        return nums[i];
-                    }
+                    high=mid-1;
+                }
+            }
+            else if(nums[mid]==nums[mid-1] && nums[mid]!=nums[mid+1]){
+                if(mid%2==0){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
                 }
             }
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1){
+//             return nums[0];
+//         }
+//         else{
+//             for(int i=0; i<n; i++){
+//                 if(i==0){
+//                     if(nums[i]!=nums[i+1]){
+//                         return nums[i];
+//                     }
+//                 }
+//                 else if(i==n-1){
+//                     if(nums[i]!=nums[i-1]){
+//                         return nums[i];
+//                     }
+//                 }
+//                 else{
+//                     if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+//                         return nums[i];
+//                     }
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
